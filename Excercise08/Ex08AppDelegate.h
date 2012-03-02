8dac9e2abd5a43934803e1f904dc644eaa37be73
NLelouche <nahuel.lelouche@threepillarglobal.com>
2012-03-02 14:59:08 -0300

    Initial Commit

---
+++ Excercise08/Ex08AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Ex08AppDelegate.h
+//  Excercise08
+//
+//  Created by TPG on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ex08AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Excercise08/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Excercise08
+//
+//  Created by TPG on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Ex08AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Ex08AppDelegate class]));
+    }
+}
